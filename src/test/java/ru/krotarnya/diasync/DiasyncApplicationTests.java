73557a6c6cc6f0d3b8a92bfc870f5cf2ec037335
ivan <illepidus@gmail.com>
2025-03-02 13:49:25 +0300

    woking generated by grok

---
+++ src/test/java/ru/krotarnya/diasync/DiasyncApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.krotarnya.diasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiasyncApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
