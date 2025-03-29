0f316970ed5d409133118e97452b34f0f9115e03
ivan <illepidus@gmail.com>
2025-03-29 12:43:04 +0300

    ping

---
+++ src/main/java/ru/krotarnya/diasync/controller/PingController.java
@@ -0,0 +1,12 @@
+package ru.krotarnya.diasync.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @RequestMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
