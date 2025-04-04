4e777bf1baf11e56cf70ba84665ff3f83cc2717a
ivan <illepidus@gmail.com>
2025-04-04 23:43:52 +0300

    VersionController

---
+++ src/main/java/ru/krotarnya/diasync/controller/VersionController.java
@@ -0,0 +1,16 @@
+package ru.krotarnya.diasync.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+    @Value("${git.commit.id:unknown}")
+    private String commitId;
+
+    @GetMapping("version")
+    public String version() {
+        return commitId;
+    }
+}
