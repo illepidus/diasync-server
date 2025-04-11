b06f6b3ba870cd7ff4074cf4d7176971afd86f58
ivan <illepidus@gmail.com>
2025-04-11 19:46:05 +0300

    minor improvement

--- src/main/java/ru/krotarnya/diasync/service/DataCleaner.java
+++ src/main/java/ru/krotarnya/diasync/service/DataCleaner.java
@@ -3,9 +3,9 @@ package ru.krotarnya.diasync.service;
 import jakarta.transaction.Transactional;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.krotarnya.diasync.repository.DataPointRepository;
@@ -13,18 +13,22 @@ import ru.krotarnya.diasync.repository.DataPointRepository;
 @Service
 public class DataCleaner {
     private static final Logger logger = LoggerFactory.getLogger(DataCleaner.class);
-    private static final int DAYS_TO_KEEP = 30;
     private final DataPointRepository dataPointRepository;
+    private final int daysToKeep;
 
-    public DataCleaner(DataPointRepository dataPointRepository) {
+    public DataCleaner(
+            DataPointRepository dataPointRepository,
+            @Value("${diasync.database.cleanup.days-to-keep}") int daysToKeep)
+    {
         this.dataPointRepository = dataPointRepository;
+        this.daysToKeep = daysToKeep;
     }
 
-    @Scheduled(fixedRate = 60, timeUnit = TimeUnit.MINUTES)
     @Transactional
+    @Scheduled(fixedDelayString = "#{T(java.time.Duration).parse('${diasync.database.cleanup.interval}').toMillis()}")
     public void cleanOldRecords() {
-        Instant before = Instant.now().minus(Duration.ofDays(DAYS_TO_KEEP));
+        Instant before = Instant.now().minus(Duration.ofDays(daysToKeep));
         int pointCount = dataPointRepository.deleteByTimestampBefore(before);
-        logger.info("Deleted {} records older than {} days at {}", pointCount, DAYS_TO_KEEP, Instant.now());
+        logger.info("Deleted {} records older than {} days at {}", pointCount, daysToKeep, Instant.now());
     }
 }
