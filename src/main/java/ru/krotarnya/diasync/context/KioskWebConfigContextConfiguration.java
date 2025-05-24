28ce41235a59072e09a69cf757c0413a2839e965
ivan <illepidus@gmail.com>
2025-05-25 00:10:50 +0300

    kiosk

---
+++ src/main/java/ru/krotarnya/diasync/context/KioskWebConfigContextConfiguration.java
@@ -0,0 +1,28 @@
+package ru.krotarnya.diasync.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+/**
+ * @author ivblinov
+ */
+
+@Configuration
+public class KioskWebConfigContextConfiguration {
+    @Bean
+    public RouterFunction<ServerResponse> kioskRouter() {
+        return RouterFunctions
+          .route(RequestPredicates.GET("/kiosk"),
+            req -> ServerResponse.ok()
+              .contentType(MediaType.TEXT_HTML)
+              .body(BodyInserters.fromResource(new ClassPathResource("static/kiosk.html")))
+          );
+    }
+}
